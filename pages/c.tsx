e4e894a24cf374c3fcef48d87c4efac8b6dc3129
Awtian <me@awtian.com>
2023-01-28 13:22:05 +0700

    initiating controller

--- pages/c.tsx
+++ pages/c.tsx
@@ -1,14 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Center, NativeSelect, Button } from '@mantine/core';
+import { rtdb } from "@/utils/firebase";
+import { onValue, ref, set } from "firebase/database";
 
 interface ColorType {
   [index:string]: string;
 }
 const color: ColorType = { blue: "#228BE6", red: "#FA5252" }
+const increment = 3;
 
 export default function Control() {
   const [team, setTeam] = useState('red');
+  const [redTeam, setRedteam] = useState(0);
+  const [blueTeam, setBlueTeam] = useState(0);
 
+  function advanceTeam(team: string) {
+    if (team === 'blue') {
+      set(ref(rtdb, '/'), {
+        blue: blueTeam + increment,
+        red: redTeam
+      });
+    } else {
+      set(ref(rtdb, '/'), {
+        blue: blueTeam,
+        red: redTeam + increment
+      });
+    }
+  }
+
+  useEffect(() => {
+    const query = ref(rtdb, "/");
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+
+      if (snapshot.exists()) {
+        setRedteam(data.red)
+        setBlueTeam(data.blue)
+      }
+    });
+  }, []);
 
   return (
     <Center style={{ height: '100vh', flexDirection: 'column', backgroundColor: color[team], rowGap: 20 }}>
@@ -18,7 +48,7 @@ export default function Control() {
         onChange={(event) => setTeam(event.currentTarget.value)}
         data={[{ value: 'red', label: 'Red Team' }, { value: 'blue', label: 'Blue Team' }]}
       />
-      <Button color="green">Confirm Team</Button>
+      <Button color="green">Run!</Button>
 
     </Center>
   )
